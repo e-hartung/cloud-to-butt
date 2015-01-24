08a08ad3830e7444fd378de177d4e1fc9fa938ff
umnothanks <elizabeth.hartung@gmail.com>
2015-01-24 16:21:11 -0500

    update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bHipster\b/g, "Hippo");
+	v = v.replace(/\bHipsters\b/g, "Hippos");
+	v = v.replace(/\bhipster\b/g, "hippo");
+	v = v.replace(/\bhipsters\b/g, "hippos");
 	
 	textNode.nodeValue = v;
 }
